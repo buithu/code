868a3193f87d1eae51ee0717074e2adf0929668c
thu <sadar19919@gmail.com>
2017-12-22 11:21:48 +0700

    model

--- application/models/Model_db.php
+++ application/models/Model_db.php
@@ -13,23 +13,11 @@ class Model_db extends CI_Model{
         $data = $query->result();
         return $data;
     }
-    public function delete($id){
-        $this->db->where('stt',$id);
-        $this->db->delete('post');
-        return true;
-    }
-    public function add($data_insert){
-        $this->db->insert('post',$data_insert);
-    }
     public function getById($id){
         $this->db->where("stt", $id);
         return $this->db->get('post')->result();
    }
    
-   public function edit($data_update, $id){
-        $this->db->where("stt", $id);
-        $this->db->update('post', $data_update);
-        
-    }
+
 }
 ?>
